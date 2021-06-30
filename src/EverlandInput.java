a79e7c59771523d7264e6a9d5222c7c3728092ad
ShinMyunggeun <86578532+ShinMyunggeun@users.noreply.github.com>
2021-06-30 09:30:06 +0900

    Update EverlandInput.java

--- src/EverlandInput.java
+++ src/EverlandInput.java
@@ -1,4 +1,4 @@
-package EVERLANDPROJECT;
+package standard;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -6,39 +6,33 @@ import java.util.Date;
 import java.util.Scanner;
 
 
-public class EverlandMain {
-	
+public class Mine2{
 	public static void main(String[] args) {
-		
-		EverlandProcess everProcess = new EverlandProcess(); 
-		ArrayListControlClass everArray = new ArrayListControlClass();
-		everProcess.whatToday();
-	
-		
-	while(true) {
-		 everArray.clearALL();
-	 do {
-    	  everProcess.inputDate();
-    	  everProcess.Pricetag();
-    	  System.out.println(everProcess.ticketType);
-  		  System.out.println("ㅡ");
-  		  everProcess.putID();
-  		  if (everProcess.registNumber.equals("0")) {continue;}
-  		  everProcess.HowBirth();
-  		  everProcess.ManyCount();
-  		  everProcess.choosePrior();
-  		  if(everProcess.Prior == 0) {continue;}
-  		  System.out.println("ㅡ");
-  		  everProcess.moreORnot();
-  		  everArray.Arrayadd(everProcess.ticketType, everProcess.age,
-  				  			everProcess.Count, everProcess.Price, everProcess.b);
-		  System.out.println("감사합니다.");
-	      System.out.println("=============== 에버랜드 =================");
+		Mine everProcess = new Mine(); 
+		Mine1 everArray = new Mine1();
+		everProcess.todayDate();	
+		while(true) {
+		everArray.clearAllData();
+		do {
+    	everProcess.inputDate();
+    	everProcess.ticketType();
+    	System.out.println(everProcess.ticketType);
+  		System.out.println("ㅡ");
+  		everProcess.givenBirth();
+  		if(everProcess.registNumber.equals("0")) {continue;}
+  		everProcess.dateBirth();
+  		everProcess.dataCount();
+  		everProcess.InputPrior();
+  		if(everProcess.Prior == 0) {continue;}
+  		System.out.println("ㅡ");
+  		everProcess.moreOver();
+  		everArray.Arrayadd(everProcess.ticketType, everProcess.age,
+  					 	   everProcess.Count, everProcess.Price, everProcess.b);
+		System.out.println("감사합니다.");
+	    System.out.println("=============== 에버랜드 =================");
       	}while(everProcess.addOrder == 1);
-      	  everArray.Result();   
-      	  break;
+      	everArray.Result();   
+      	break;
+		}
 	}
-     
-}
-
 }
