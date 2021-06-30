95ad86ad247f3fbac9838c8c4fdc0d1c0e9ecf1b
ShinMyunggeun <86578532+ShinMyunggeun@users.noreply.github.com>
2021-06-30 13:41:55 +0900

    Add files via upload

---
+++ src/EverDataClass.java
@@ -0,0 +1,11 @@
+package standard;
+
+public class DataClass {
+
+	public String ticketType = null;
+	public int Count = 0;
+	public int Price = 0;
+	public String Age = null;
+	public String B = null;
+	public String isEventcoupon = null;
+	}
